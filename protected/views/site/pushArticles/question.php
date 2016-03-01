3b41ed9dec52a459609ad01654292430b9dfe25c
Vincen Wen <vincen.wen@samesamechina.com>
2016-03-01 16:45:51 +0800

    no message

--- protected/views/site/pushArticles/question.php
+++ protected/views/site/pushArticles/question.php
@@ -31,8 +31,8 @@
             <dl>
                 <dt>A:</dt>
                 <dd>
-                    法国春天百货奥斯曼旗舰店为您提供3000多个国际品牌，其中包括春天百货独家品牌以及汊流买手店Maria Luisa。请<a href="http://printempsb2b.samesamechina.com/site/brand?b=1" target="_blank">点击此处</a>查询完整的品牌目录。<br />
-                    法国春天百货卢浮宫分店为您提供一系列的奢侈品配饰、高级腕表及美妆精选品牌。请<a href="http://printempsb2b.samesamechina.com/site/brand?b=2" target="_blank">点击此处</a>查询完整的品牌目录。
+                    法国春天百货奥斯曼旗舰店为您提供3000多个国际品牌，其中包括春天百货独家品牌以及汊流买手店Maria Luisa。请<a href="http://printempsb2b.samesamechina.com/site/brand?b=1">点击此处</a>查询完整的品牌目录。<br />
+                    法国春天百货卢浮宫分店为您提供一系列的奢侈品配饰、高级腕表及美妆精选品牌。请<a href="http://printempsb2b.samesamechina.com/site/brand?b=2">点击此处</a>查询完整的品牌目录。
                 </dd>
             </dl>
         </div>
@@ -58,7 +58,7 @@
                     75001 Paris - France <br />
                     地铁: 1号线，7号线 Palais Royal Musée du Louvre
                     公交线路: 21，95，<br />
-                    请<a href="http://printempsb2b.samesamechina.com/site/sitmap" target="_blank">点击此处</a>地理定位。
+                    请<a href="http://printempsb2b.samesamechina.com/site/sitmap">点击此处</a>地理定位。
 
                 </dd>
             </dl>
@@ -126,7 +126,7 @@
             <dl>
                 <dt>A:</dt>
                 <dd>
-                    请您<a href="http://printempsb2b.samesamechina.com/site/sitmap" target="_blank">点击此处</a>查找法国春天百货在法国所有门店的地理位置。
+                    请您<a href="http://printempsb2b.samesamechina.com/site/sitmap">点击此处</a>查找法国春天百货在法国所有门店的地理位置。
                 </dd>
             </dl>
         </div>
--- protected/views/site/sitmap.php
+++ protected/views/site/sitmap.php
@@ -13,19 +13,6 @@
 		按“+”按钮,推送您的地址信息<br />即可获得您附近 春天百货 Printemps专卖店位置
 	</div>
 
-//	<div class="infoList">
-//		<h2>布雷斯特店（BREST ）</h2>
-//		<p><em>地址:</em> <span>59, RUE JEAN JAURÈS 29200   BREST  -  FRANCE</span></p>
-//		<p><em>电话:</em> <span>0033 2 98 44 65 65</span></p>
-//		<p><em>营业时间:</em> <span>周一至周六 9:30至19:30</span></p>
-//	</div>
-//
-//	<div class="infoList">
-//		<h2>卡昂（CAEN ）</h2>
-//		<p><em>地址:</em> <span>28, RUE ST JEAN 14000   CAEN   -   FRANCE</span></p>
-//		<p><em>电话:</em> <span>0033 2 31 15 65 50</span></p>
-//		<p><em>营业时间:</em> <span>周一至周五 9:30至19:00  周六 9:35至20:00</span></p>
-//	</div>
 
 </section>
 </article>
