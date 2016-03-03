cb6e9525f938cc24d9c51f1f7decc0430a864fc8
Vincen Wen <vincen.wen@samesamechina.com>
2016-03-03 11:07:29 +0800

    no message

--- protected/views/site/sitmap.php
+++ protected/views/site/sitmap.php
@@ -16,16 +16,16 @@
 
 </section>
 </article>
-<footer>
+<!-- <footer>
 	<div class="con">
-		<a href="javascript:;" id="tel"> 
+		<a href="javascript:;" id="tel">
 			<img src="/vstyle/imgs/tel.png" />
-			<u>+33 142825000</u> 
+			<u>+33 142825000</u>
 		</a>
 		<em>联系方式</em>
 	</div>
 	<img src="/vstyle/imgs/footer.jpg" width="100%" class="opacity" />
-</footer>
+</footer> -->
 
 <script type="text/javascript">
 
