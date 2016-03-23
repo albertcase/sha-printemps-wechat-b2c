231386eb0c597c45cc33c07256d7a1d130f59751
Vincen Wen <vincen.wen@samesamechina.com>
2016-03-23 11:17:58 +0800

    no message

--- protected/views/site/pushArticles/restaurant.php
+++ protected/views/site/pushArticles/restaurant.php
@@ -4,7 +4,7 @@
 
         <div class="infocontext">
 
-
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/brasserie_printemps.jpg" width="100%" />
             <p>
             <strong>春天百货彩绘穹顶餐厅:</strong><br />
             法国春天百货高达50米的圆形穹顶是在1910年由建筑师René Binet设计并由3125块彩绘玻璃构建而成的。这久负盛名的具有装饰艺术风格的圆形穹顶如今被改造成新艺术风格的优雅法式餐厅。
@@ -12,18 +12,19 @@
             电话：+33 (0)1 42 82 58 84<br />
             春天女士时尚馆，6层
             </p>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/brasserie_printemps.jpg" width="100%" />
 <br /><br />
 
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/le-delicieux_630.jpg" width="100%" />
             <p>
             <strong>巴黎360°观景咖啡厅:</strong><br />
             这个矗立在天与地之间的美妙场所，可以让您站在春天百货的露台上饱览巴黎历史名胜以及无以伦比的景致。<br />
             电话：+33 (0)1 42 82 62 76<br />
             春天美容-家居馆，9层
             </p>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/le-delicieux_630.jpg" width="100%" />
+
 <br /><br />
 
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/laduree_630.jpg" width="100%" />
             <p>
             <strong>Ladurée甜品沙龙:</strong><br />
             大名鼎鼎的Ladurée法式甜品品牌在拿破仑三世风格的沙龙里恭候您的光临，让您尽享精致、甜美、咸淡宜口的休憩时光。您可以在茶点时间品尝这里风味独特的法式甜品马卡龙。
@@ -32,35 +33,38 @@
             春天女士时尚馆，0层及1层<br />
             春天美容家居馆，2层
             </p>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/laduree_630.jpg" width="100%" />
+
 <br /><br />
 
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/cojean_630.jpg" width="100%" />
             <p>
-            <strong>COJEAN:</strong><br />
+            <strong>Cojean:</strong><br />
             健康，有机，美食新主张<br />
             可人有机食品店<br />
             在冰蓝的装饰色调中，享受Cojean为您提供的健康有机食品，蔬菜、水果、沙拉均衡搭配。<br />
-            电话：+33 1 42 82 40 25<br />
+            电话：+33 (0)1 42 82 40 25<br />
             奥斯曼旗舰店，女士时尚馆，-1层
             </p>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/cojean_630.jpg" width="100%" />
+
 <br /><br />
 
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/pouchkine.jpg" width="100%" />
             <p>
             <strong>普希金点心店:</strong><br />
             普希金点心店是不可错过的莫斯科风味甜品名店，您可以在这里品尝到各式各样、超凡美味的俄式精美点心小吃。<br />
             电话：+33 (0)1 42 82 43 31<br />
             春天女士时尚馆，0层
             </p>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/pouchkine.jpg" width="100%" />
+
 <br /><br />
 
+            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/linas.jpg" width="100%" />
             <p>
             <strong>Lina's餐厅:</strong><br />
             Lina's餐厅为您提供一系列每日以新鲜食材悉心烹饪的美食，从美味法式三明治到时令沙拉再到美味的果汁，种类繁多，搭配新颖。这里是放松休息片刻享受美食的不二之选。
             春天美容-家居馆，3层
             </p>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/pushArticles/restaurant/linas.jpg" width="100%" />
+
 <br /><br />
 
         </div>
