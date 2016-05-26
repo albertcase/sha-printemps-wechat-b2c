fb95ba443e7a83d40f948006b60da01628d9833c
Vincen Wen <vincen.wen@samesamechina.com>
2016-05-26 10:46:24 +0800

    no message

--- protected/views/site/pushArticles/cba.php
+++ protected/views/site/pushArticles/cba.php
@@ -1,6 +1,6 @@
 <div class="page store">
     <div class="container">
-        <h2>商务合作咨询</h2>
+        <h2>商务合作资讯</h2>
 
 	    <div class="infocontext">
 	        <p>春天百货国际部温馨提醒各位已在我司注册的商务合作伙伴每年必须更新一次注册信息，此信息未更新的情况下您合作款的支付将被延迟。请各位还没有更新的合作伙伴来我们国际部进行更新或通过此微信将需要更新的资料上传。除了身份证件其他资料都可以通过此微信发送。请您根据以下归类准备您的更新资料。</p>
@@ -13,7 +13,9 @@
             <p>
                 2.  长期居住法国的自由职业者信息年度更新要求：<br />
                 -   请在我司填写更新登记表格 <br />
-                -   3 个月内出具的 Situation au répertoire SIRENE de l'INSEE • 3 个月内出具的 URSSAF 或 RSI 的 attestation de paiement des cotisations sociales • 有效居留卡或法籍身份证 原件以备复印 <br />
+                -   3 个月内出具的 Situation au répertoire SIRENE de l'INSEE <br />
+                -   3 个月内出具的 URSSAF 或 RSI 的 attestation de paiement des cotisations sociales <br />
+                -   有效居留卡或法籍身份证 原件以备复印 <br />
                 -   如果您的账户信息有变提交新的银行账户信息
             </p>
             <p>
