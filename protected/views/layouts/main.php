88d9e55e97ac9fae92f5986419557a3c5c0c6ca2
Vincen Wen <vincen.wen@samesamechina.com>
2016-06-20 17:12:11 +0800

    no message

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -16,19 +16,29 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/css/style.css" />
 
 	<script>
-	var _hmt = _hmt || [];
-	(function() {
-	  var hm = document.createElement("script");
-	  hm.src = "//hm.baidu.com/hm.js?3dada998823fa0a33181a63f2b1c2d3b";
-	  var s = document.getElementsByTagName("script")[0]; 
-	  s.parentNode.insertBefore(hm, s);
-	})();
+	// var _hmt = _hmt || [];
+	// (function() {
+	//   var hm = document.createElement("script");
+	//   hm.src = "//hm.baidu.com/hm.js?3dada998823fa0a33181a63f2b1c2d3b";
+	//   var s = document.getElementsByTagName("script")[0]; 
+	//   s.parentNode.insertBefore(hm, s);
+	// })();
 	</script>
 
 	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/js/jquery.js"></script>
 </head>
 <body>
 	<?php echo $content; ?>
+	
+<script type="text/javascript">
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-9680728-5', 'auto');
+	ga('send', 'pageview');
+</script>
 </body>
 </html>
 
