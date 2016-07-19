be75870ff37937fb1bf6a546cab75297cc72c81c
Vincen Wen <vincen.wen@samesamechina.com>
2016-07-19 17:55:02 +0800

    no message

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -6,21 +6,19 @@
 			<img src="<?php echo Yii::app()->request->baseUrl; ?>/vstyle/imgs/logo.png" width="100%" />
 			<div class="loginframe" style="display:inline-block;">
 				<div class="login_tips">
-					欢迎使用<br />
-                    法国春天百货导游服务账号<br />
-                    <span>请通过您的春天商务编号与<br />
-                    您的姓氏登录，谢谢！</span>
-
+					<b style="font-size:16px;">欢迎关注</b><br />
+					法国春天百货PRINTEMPS商务微信号<br />
+                    <span>此微信号仅用于商务交流，如果您是春天百货的客人，请关注春天百货的官方公众号。如果您是春天百货的商务伙伴，请通过您的春天商务编号与您的姓氏拼音登录，谢谢！</span>
 				</div>
 
 				<div class="login_form">
 					<ul>
 						<li>
-							<p>春天商务编号： </p>
+							<p>春天商务编号：</p>
 							<input type="text" name="code">
 						</li>
 						<li>
-							<p>姓氏：</p>
+							<p>姓氏拼音：</p>
 							<input type="text" name="name">
 						</li>
 					</ul>
@@ -73,9 +71,9 @@
 		$("input").val("");
 		funTips(callbackTips);
 	}
-	function funTips(callBt){
+	function funTips(callBt, _time){
 		$(".login_tips").addClass("error").html(callBt);
-		setTimeout('$(".login_tips").removeClass("error").html("欢迎使用<br />法国春天百货导游服务账号<br />请你通过导游证编号与姓名登录<br />谢谢！");', 6000)
+		setTimeout('$(".login_tips").removeClass("error").html("<b style="font-size:16px;">欢迎关注</b><br />法国春天百货PRINTEMPS商务微信号<br /><span>此微信号仅用于商务交流，如果您是春天百货的客人，请关注春天百货的官方公众号。如果您是春天百货的商务伙伴，请通过您的春天商务编号与您的姓氏拼音登录，谢谢！</span>");', _time)
 	}
 
 	function checkForm(){
@@ -83,9 +81,9 @@
 		var name = $("input[name=name]").val();
 
 		if(codenum == ""){
-			funTips("请输入导游证编号！");
+			funTips("请输入春天商务编号！", "1000");
 		}else if(name == ""){
-			funTips("请输入姓名！");
+			funTips("请输入姓氏拼音！", "1000");
 		}else{
 
 			$.ajax({
@@ -103,11 +101,11 @@
 		    		$("#successTips").css({"display":"inline-block"});
 		    		callbackTips = "验证成功";
 		    	}else{
-		    		callbackTips = "<em>"+data.msg+"</em><p>如需要联系法国春天百货国际部</p><p>请致电：</p><p><a href='tel:+33142825579'>+33 1 42 82 55 79</a> <br /><a href='tel:+33142825580'>+33 1 42 82 55 80</a></p>";
+		    		callbackTips = "<em>很抱歉，登陆失败<br>请重新输入</em><p style='font-size:16px; line-height:22px;'>如需要联系法国春天百货国际部<br>请致电：<a href='tel:+33142825579'>+33 1 42 82 55 79</a> <br /><a href='tel:+33142825580'>+33 1 42 82 55 80</a></p><br>";
 		    	}
 		    	
 				$("input").val("");
-				funTips(callbackTips);
+				funTips(callbackTips, "6700");
 		    })
 			
 		}
